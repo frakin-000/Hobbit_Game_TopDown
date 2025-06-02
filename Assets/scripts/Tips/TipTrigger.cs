8660f13d4b0c9eff49a702aa71265d799bb40e3b
Андрей <Spirin.Andrei@urfu.me>
2025-06-02 21:13:51 +0500

    final add

--- Assets/scripts/Tips/TipTrigger.cs
+++ Assets/scripts/Tips/TipTrigger.cs
@@ -4,13 +4,23 @@ public class TipTrigger : MonoBehaviour
 {
     [SerializeField] private string message;
 
-    private void OnTriggerEnter2D(Collider2D other)
+
+
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if (other.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
             TipManager.displayTipEvent?.Invoke(message);
         }
     }
+    //private void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        TipManager.displayTipEvent?.Invoke(message);
+    //    }
+    //}
 
     private void OnTriggerExit2D(Collider2D other)
     {
